780980a8105f156acd35d58bf1b8a1957868653a
Enise Ahsen Karadağ <140591106+eniseahsen@users.noreply.github.com>
2026-04-12 15:22:56 +0300

    Delete inheritance directory

--- inheritance/Animal.kt
+++
@@ -1,9 +0,0 @@
-package inheritance
-
-//Kotlin’de bir sınıfı başka bir sınıfa miras verebilmek istiyorsan o sınıf open olmak zorunda.
-open class Animal {
-    fun eat(){
-        println("Animal is eating")
-    }
-}
-
--- inheritance/Dog.kt
+++
@@ -1,8 +0,0 @@
-package inheritance
-
-class Dog: Animal() {
-    fun bark(){
-        println("Dog is barking")
-    }
-
-}
